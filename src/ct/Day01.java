b9936e90410613aa2231186b85f0e2e8e3f4bbe2
ministar <oblilic93@gmail.com>
2023-04-26 00:26:35 +0900

    20230426

--- src/ct/Day01.java
+++ src/ct/Day01.java
@@ -3,6 +3,11 @@ package ct;
 public class Day01 {
     //사칙연산
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       int res = 1;
+       for(int i=0; i< 10; i++){
+           res = res * 2;
+           System.out.println( i + "=" + res);
+       }
+        System.out.println(res);
     }
 }
