42d32dcfe397a16f69188c1f88a568b73dfc8b55
ministar <oblilic93@gmail.com>
2023-05-24 23:06:17 +0900

    20230510

---
+++ src/ct/First12.java
@@ -0,0 +1,10 @@
+package ct;
+
+public class First12 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
